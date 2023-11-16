3e46d2d8528f5e553585a9ba16608cbdf1858a68
Minh <achillesback0511@yahoo.com>
2023-11-16 19:15:29 +0700

    third

--- store.sql
+++ store.sql
@@ -16,13 +16,13 @@ INSERT INTO `items` (`id`, `name`, `price`) VALUES
 (3, 'Sony DSLR', 450),
 (4, 'Olympus DSLR', 500),
 (5, 'Titan Model #301', 1300),
-(6, 'Titan Model #201', 500),
-(7, 'HMT Milan', 800),
+(6, 'Titan Model #201', 3000),
+(7, 'HMT Milan', 8000),
 (8, 'Faber Luba #111', 1800),
-(9, 'Marshall Major IV', 120),
-(10, 'Sony WH-XB910N', 250),
-(11, 'Jabra Evolve 80', 259),
-(12, 'Beats Studio3', 349);
+(9, 'H&W', 800),
+(10, 'Luis Phil', 1000),
+(11, 'John Zok', 1500),
+(12, 'Jhalsani', 1300);
 
 
 CREATE TABLE `users` (
@@ -36,7 +36,13 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `contact`, `city`, `address`) VALUES
-(1, 'Minh', 'Abc123@gmail.com', 'Asdasd123', '0123456789', 'HoChiMinh', 'ABC'),
+(1, 'Vishwa', 'vish@gmail.com', '708175b3fdb269c4ebe8e7751bb3fccd', '9283746352', 'Bengaluru', 'JP Nagar 2nd Phase'),
+(2, 'Vishal', 'vishal@gmail.com', '72eea11febe23ae3dcab2bf59794c54c', '9120394750', 'Chennai', 'T Nagar'),
+(3, 'Ram', 'ram@gmail.com', '1b7b4c38f626766bbdcfc895e2c514f6', '9718273679', 'Delhi', 'PMO'),
+(4, 'Raj', 'raj@gmail.com', '1537c1dea8479ff52bc68336e323385f', '9281736475', 'Bengaluru', 'Basavangudi'),
+(5, 'Ravi', 'ravi@gmail.com', 'f4690de2554616f5b853d35f5a7970d4', '9182712098', 'Chennai', 'abc'),
+(6, 'Vishwadutt', 'vishwadutt99@gmail.com', '8231ac09972b51e692a5f515f8349a29', '9741301718', 'Bengaluru', 'Jayanagar 3rd Block');
+
 
 CREATE TABLE `user_item` (
   `id` int(11) NOT NULL,
@@ -72,4 +78,3 @@ ALTER TABLE `users`
 
 ALTER TABLE `user_item`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-COMMIT;
